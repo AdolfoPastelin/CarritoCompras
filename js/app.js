e5c6b00a5b1068292fe68efd62fd3100ad536df1
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-02-02 20:32:22 -0600

    variables a usar para eventos de click.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Variables a usar para eventos click //
+const listaCursos = document.querySelector('#lista-cursos');
+//contenedor de lista del carrito
+const listaCarrito = document.querySelector('#lista-carrito tbody');
+//boton para eliminar cursos del carrito
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+// arreglo vacio que se irá llenando
+let articulosCarrito = [];
+
